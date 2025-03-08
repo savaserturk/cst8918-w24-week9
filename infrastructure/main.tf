12d1f90ed09ccda5760c6a46b1078081dec903e3
Savas Erturk <ertu0002@algonquinlive.com>
2025-03-07 23:39:14 -0500

    Test Husky pre-commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ertu0002-tf-rg"
+  location = "westus3"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfstoragedemo20251995" 
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
